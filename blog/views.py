ba04103e74e359c798da62d59551ff304e9efd9f
Nebelherr <dracon23@outlook.de>
2015-07-31 21:25:07 +0200

    [blog] Removed unneccessary logging and imports from view files

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Blogpost, Tag
+from .models import Blogpost
 from assets.settings import base as sets
-import logging
 # Create your views here.
 
 class GetNewsPosts(generic.ListView):
@@ -18,19 +17,14 @@ class GetNewsPosts(generic.ListView):
 class GetSinglePost(generic.DetailView):
 
     def get(self, request, p_id, *args, **kwargs):
-        logger = logging.getLogger('project')
         post = Blogpost.objects.get(id=p_id)
         tags = post.tags.select_related()
-        logger.debug('Tags: {}'.format(post.tags))
         ctx = {'post': post, 'tags': tags}
         return render(request, 'blog/view_single.html', ctx)
 
 class GetPostsByTag(generic.ListView):
 
     def get(self, request, tag_id, *args, **kwargs):
-        logger = logging.getLogger('project')
-        logger.debug('Getting posts with tag id {} assigned'.format(tag_id))
         posts = Blogpost.objects.all().filter(tags__id=tag_id)
-        logger.debug('Found {} posts with tag id {}'.format(posts.count(), tag_id))
         ctx = {'posts': posts}
         return render(request, 'blog/view_latest.html', ctx)
